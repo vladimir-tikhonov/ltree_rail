d84b756e95f0b5750e84b286a1b45acf873673e5
v.tikhonov <vladimir@tikhonov.by>
2015-03-03 07:23:27 +0300

    Cleanup some tests

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -2,46 +2,42 @@ describe LtreeRails::Model do
   let(:root_category) { Category.create! }
 
   it 'adds parent relation' do
-    c = Category.new(parent_id: root_category.id)
-    c.save!
+    c = Category.create!(parent_id: root_category.id)
     expect(c.parent).to eq root_category
   end
 
   it 'adds category relation' do
-    c1 = Category.new(parent_id: root_category.id)
-    c1.save!
-    c2 = Category.new(parent_id: root_category.id)
-    c2.save!
+    c1 = Category.create!(parent_id: root_category.id)
+    c2 = Category.create!(parent_id: root_category.id)
     expect(root_category.children).to eq [c1, c2]
   end
 
   context 'root?' do
     context 'when persisted' do
       it 'returns true when parent column value is nil' do
-        expect(root_category.root?).to be true
-        expect(root_category.child?).to be false
+        expect(root_category).to be_root
+        expect(root_category).not_to be_child
       end
 
       it 'returns false when parent column value is not nil' do
-        c = Category.new(parent_id: root_category.id)
-        c.save!
-        expect(c.root?).to be false
-        expect(c.child?).to be true
+        c = Category.create!(parent_id: root_category.id)
+        expect(c).not_to be_root
+        expect(c).to be_child
       end
     end
 
     context 'when new record' do
       it 'returns true if parent is nil' do
         c = Category.new
-        expect(c.root?).to be true
-        expect(c.child?).to be false
+        expect(c).to be_root
+        expect(c).not_to be_child
       end
 
       it 'returns false if parent is not nil' do
         c = Category.new
         c.parent = root_category
-        expect(c.root?).to be false
-        expect(c.child?).to be true
+        expect(c).not_to be_root
+        expect(c).to be_child
       end
     end
   end
